c680fbd6d54b1b6e39750af3b832443f12abed9c
knahdiyaF <42751818+knahdiyaF@users.noreply.github.com>
2022-03-01 22:06:59 -0600

    Add files via upload

---
+++ PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  InstagramApp
+//
+//  Created by Knahdiya Flood on 2/28/22.
+//
+
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
